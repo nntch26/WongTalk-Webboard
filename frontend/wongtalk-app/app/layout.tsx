c56aab855e9f3b02542f49d3bff62ef1e3450d5a
Natcha Nernkrang <117381190+nntch26@users.noreply.github.com>
2025-02-24 11:41:42 +0700

    add time post detail

--- frontend/wongtalk-app/app/layout.tsx
+++ frontend/wongtalk-app/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Kanit } from 'next/font/google'; // ฟอนต์
 import '@fortawesome/fontawesome-free/css/all.css';
